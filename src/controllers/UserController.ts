2040ccdb81fdd9d92db51e9331601786d4523949
gustas01 <gustasboyevolution@hotmail.com>
2023-02-28 15:37:12 -0300

    fazendo crud de usuários

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,8 +1,20 @@
 import { Request, Response } from 'express';
 import { IErrorDB } from '../interfaces/IErrorDB';
+import { IUser } from '../interfaces/IUser';
 import User from '../models/User';
 
 class UserController {
+  async index(req: Request, res: Response){
+    try{
+      const users = await User.findAll({attributes: ['id', 'name', 'email']});
+
+      return res.json(users);
+    }catch(e: any){
+      res.status(400).json({errors: e.errors.map((err: IErrorDB) => err.message)});
+    }
+  }
+
+
   async create(req: Request, res: Response){
     try{
       const { password } = req.body;
@@ -11,9 +23,61 @@ class UserController {
         return res.status(400).json({errors: ['A senha deve ter pelo menos 1 letra maiúscula e 1 minúscula']});
 
 
-      const newUser = await User.create(req.body);
+      const newUser = await User.create(req.body) as unknown as IUser;
+      console.log(newUser);
 
-      res.json(newUser);
+      const {id, name, email} = newUser;
+      res.json({id, name, email});
+    }catch(e: any){
+      res.status(400).json({errors: e.errors.map((err: IErrorDB) => err.message)});
+    }
+  }
+
+
+  async read(req: Request, res: Response){
+    try{
+      const user = await User.findByPk(req.params.id) as unknown as IUser;
+
+      if(!user) return res.status(400).json({errors: ['Usuário não encontrado']});
+
+      const {id, name, email} = user;
+
+      res.json({id, name, email});
+    }catch(e: any){
+      res.status(400).json({errors: e.errors.map((err: IErrorDB) => err.message)});
+    }
+  }
+
+
+  async update(req: Request, res: Response){
+    try{
+      const user = await User.findByPk(req.params.id);
+
+      if(!user) return res.status(400).json({errors: ['Usuário não encontrado']});
+
+      // const updatedUser = await User.update(req.body, { where: { id: req.params.id }});
+      const updatedUser = await user.update(req.body) as unknown as IUser;
+
+      const { id, name, email } = updatedUser;
+
+      return res.json({ id, name, email });
+
+    }catch(e: any){
+      res.status(400).json({errors: e.errors.map((err: IErrorDB) => err.message)});
+    }
+  }
+
+
+  async delete(req: Request, res: Response){
+    try{
+      const user = await User.findByPk(req.params.id);
+
+      if(!user) return res.status(400).json({errors: ['Usuário não encontrado']});
+
+      const deletedUser = await  User.destroy({where: {id: req.params.id}});
+
+      return res.json('Usuário deletado com sucesso!');
+
     }catch(e: any){
       res.status(400).json({errors: e.errors.map((err: IErrorDB) => err.message)});
     }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,6 +3,10 @@ import userController from '../controllers/UserController';
 
 const router = Router();
 
+router.get('/', userController.index);
 router.post('/', userController.create);
+router.get('/:id', userController.read);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
